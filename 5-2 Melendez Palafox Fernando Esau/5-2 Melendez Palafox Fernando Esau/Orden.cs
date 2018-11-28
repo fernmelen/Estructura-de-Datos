c1e822e57d51aecb15e9f3a1ca8023419a1b8179
fernmelen <43394936+fernmelen@users.noreply.github.com>
2018-11-28 21:05:56 +1030

    Add files via upload

---
+++ 5-2 Melendez Palafox Fernando Esau/5-2 Melendez Palafox Fernando Esau/Orden.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_Melendez_Palafox_Fernando_Esau
+{
+    class Orden
+    {
+        public void QuickSort(double[] numeros, int menor, int mayor)  //metodo requerido
+        {
+            int aux = menor, aux2 = mayor, pivote = (mayor+menor) / 2; double temp;  //axiliares mas operaciones
+            do
+            {
+                while (numeros[aux] < numeros[pivote]) { aux++; }  //Comparar para dejar a los menores del lado izquierdo
+                while (numeros[aux2] > numeros[pivote]) { aux2--; } //Comparar para dejar a los mayores a la derecha
+                if (aux <= aux2)  //comparar el menor con el mayor hasta que se intercepten termina
+                {
+                    temp = numeros[aux];  //guardamos el numero mayor en la temporal
+                    numeros[aux] = numeros[aux2];  //los igualamos al menor ambos
+                    numeros[aux2] = temp;   //del lado derecho dejamos al mayor
+                    aux++; aux2--;  //acercamos mas las auxiliares que eran los limites del arreglo
+                }
+            } while (aux <= aux2);  //condicion de ejecucion hasta que los auxiliares de limites se intercepten
+            if (mayor > aux) { QuickSort(numeros, aux, mayor); }  //recursividad del metodo para reducir el pivote en medio de los mayores
+            if (menor < aux2) { QuickSort(numeros, menor, aux2); }  //recursividad igual pero con los menores
+        }
+        public void Imprimir(double[] ordenados)
+        {
+            QuickSort(ordenados, 0, ordenados.Length-1);  //volvemos a llamar al metodo una vez mas para corregir los errores 
+            for (int i = 0; i < ordenados.Length; i++)
+            {
+                Console.Write("| " + ordenados[i] + " |");// imprimir cada elemento del arreglo una vez ya ordenado
+            }
+        }
+    }
+}
---
+++ 5-2 Melendez Palafox Fernando Esau/5-2 Melendez Palafox Fernando Esau/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_Melendez_Palafox_Fernando_Esau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] primero = { 11, 2, 3, 14, 18, 45, -3, -1, 6, 9 };  //hacemos todos los arreglos precargados
+            double[] segundo = { 1, 3, 5, 7, 8, 3, 9, .7071, 16.5664, 12, 0, 10 };
+            double[] tercero = { 3, 7, 15, 4 / 3, 155, 100, 15, (3)^2, 5, 2, 3, 2 };
+            double[] cuarto = { 8, 19, 7, 3, 15, 23, 34, 27, 89, 101 };
+            Orden Usuario1 = new Orden();  //creamos el objeto para ejecutar sus metodos
+            Console.WriteLine("Numeros ordenados de primer arreglo");
+            Usuario1.QuickSort(primero, 0, 9);Usuario1.Imprimir(primero);  //llamamos a ambos metodos para cada arreglo
+            Console.WriteLine("\nNumeros ordenados de segundo arreglo");
+            Usuario1.QuickSort(segundo, 0, 11); Usuario1.Imprimir(segundo);
+            Console.WriteLine("\nNumeros ordenados de tercer arreglo");
+            Usuario1.QuickSort(tercero, 0, 11); Usuario1.Imprimir(tercero);
+            Console.WriteLine("\nNumeros ordenados de cuarto arreglo");
+            Usuario1.QuickSort(cuarto, 0, 9); Usuario1.Imprimir(cuarto);
+            Console.ReadKey();
+        }
+    }
+}
